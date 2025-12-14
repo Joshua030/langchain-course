c21fc621ba270a18ecf388ca7501555557561177
Jose Luis <joseluis.ceballos@techfriendly.es>
2025-12-14 01:55:00 +0100

    hello world chain

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-import os
-
 from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from  langchain_ollama import ChatOllama
+
 
 load_dotenv()
 
-
 def main():
     print("Hello from langchain-course!")
-    print("Your OpenAI API Key is:", os.getenv("OPENAI_API_KEY"))
+    information = """
+    Elon Reeve Musk (Pretoria, 28 de junio de 1971) es un empresario, inversor, activista político conservador[3][4] y magnate.[nota 1]​ Es el fundador, consejero delegado e «ingeniero» en jefe de la empresa SpaceX; inversor ángel, director general y arquitecto de productos de Tesla, Inc.; fundador de The Boring Company; y cofundador de Neuralink y OpenAI.[nota 2]​ Además, es el director de tecnología de X Corp.[5]​ Entre enero y mayo de 2025, ejerció como administrador de facto del Departamento de Eficiencia Gubernamental de la Casa Blanca bajo la segunda presidencia de Donald Trump.[6]​[7]​
 
+    Con un patrimonio neto estimado en poco más de 450 mil millones de dólares en octubre de 2025,[8]​ es la persona más rica del mundo según el índice de multimillonarios en tiempo real de Forbes.[9]​[10]​
+
+    Musk nació y se crio en una rica familia de Pretoria (Sudáfrica). Su madre es canadiense y su padre un sudafricano blanco. Estudió brevemente en la Universidad de Pretoria antes de trasladarse a Canadá a los 17 años. Se matriculó en la Universidad de Queen y se trasladó a la Universidad de Pensilvania dos años después, donde se graduó en Economía y Física. En 1995 se trasladó a California para asistir a la Universidad Stanford, pero en su lugar decidió seguir una carrera empresarial, cofundando la empresa de software web Zip2 con su hermano Kimbal. Zip2 fue adquirida por Compaq por 307 millones de dólares en 1999. Ese mismo año, Musk cofundó el banco en línea X.com, que se fusionó con Confinity en 2000 para formar PayPal. La empresa fue comprada por eBay en 2002 por mil quinientos millones de dólares.
+    """
+
+    summary_template = """
+    given the information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    # Fix: Use from_template method
+    summary_prompt_template = ChatPromptTemplate.from_template(summary_template)
+
+    #llm = ChatOpenAI(temperature=0, model_name="gpt-4")
+    llm = ChatOllama(temperature=0, model="gemma3:270m")
+    chain = summary_prompt_template | llm
+    response = chain.invoke(input={"information": information})
+    print("Response from the LLM:")
+    print(response.content)
 
 if __name__ == "__main__":
     main()
