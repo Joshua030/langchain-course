71a64640f8bc7b27d5bfdcab14dc1c89598b1761
Jose Luis <joseluis.ceballos@techfriendly.es>
2025-12-23 15:21:12 +0100

    Add tacily to the project

--- react-search-agent/main.py
+++ react-search-agent/main.py
@@ -5,32 +5,35 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+# from tavily import TavilyClient
 
 
 
-@tool
-def search(query: str) -> str:
-    # Simulate a search operation
-    """
-    Docstring for search
-    
-    :param query: Description
-    :type query: str
-    :return: Description
-    :rtype: str
-    """
-    print(f"Searching for: {query}")
-    return "Tokio weather is sunny with a high of 25°C."
+# Manual search tool implementation
+# tavily_client = TavilyClient()
+
+# @tool
+# def search(query: str) -> str:
+#     """
+#     Tool that searches over internet
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The search result
+#     """
+#     print(f"Searching for {query}")
+#     return tavily_client.search(query=query)
 
 # Create the agent with the search tool
 model = ChatOpenAI(
     model="gpt-5",
     temperature=0.1,
-    max_tokens=1000,
-    timeout=30
+    # max_tokens=1000, // set the ampit of tokens you want in response
+    timeout=60
     # ... (other params)
 )
-tools = [search]
+tools = [TavilySearch()]
 
 # agent = create_agent("gpt-5", tools=tools)
 agent = create_agent(model, tools=tools)
@@ -41,7 +44,7 @@ def main():
 #     {"messages": [{"role": "user", "content": "What's the weather in San Francisco?"}]}
 # )
     result = agent.invoke(
-    {"messages": [  HumanMessage("What's the weather in San Francisco?")]}
+    {"messages": [  HumanMessage("search for 3 job postings for an aiengineer using langchain in the bay area on linkedin and list their details")]}
 )
     print("Agent response:", result)
 
